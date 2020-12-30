e335f99c9e25b839be456c583880d0a2cd40814c
Vasili Karaev <kratos661@gmail.com>
2020-12-31 02:34:00 +0300

    add *.svg typings

--- app/typings.d.ts
+++ app/typings.d.ts
@@ -1,3 +1,13 @@
+declare module '*.svg' {
+  import React from 'react'
+
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
+
+  const src: string
+
+  export default src
+}
+
 declare module '*.scss' {
   export const content: { [className: string]: string }
 
