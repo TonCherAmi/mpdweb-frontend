67d6c9f66bfbf9aca60c0207b03a76f3d15c27e3
Vasili Karaev <kratos661@gmail.com>
2022-01-31 20:04:17 +0300

    display current song

--- app/status/components/CurrentSong/index.tsx
+++ app/status/components/CurrentSong/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
 
+import * as R from 'ramda'
+
+import useStatusContext from '@app/status/use/useStatusContext'
+import usePlaylistContext from '@app/playlist/use/usePlaylistContext'
+
 import styles from './styles.scss'
 
 const CurrentSong = () => {
+  const status = useStatusContext()
+  const playlist = usePlaylistContext()
+
+  if (R.isNil(status) || R.isNil(status.currentSong)) {
+    return null
+  }
+
+  const title = playlist[status.currentSong]
+    ?.title
+    ?? '-------'
+
+  const artist = playlist[status.currentSong]
+    ?.artist
+    ?? '---'
+
   return (
     <div className={styles.container}>
       <span className={styles.title}>
-      -------
+      {title}
       </span>
       <span className={styles.artist}>
-      ---
+      {artist}
       </span>
     </div>
   )
