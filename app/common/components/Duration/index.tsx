6eeda69390eb23167fa0c829fe6fba7e78da54de
Vasili Karaev <kratos661@gmail.com>
2022-05-07 11:07:47 +0300

    display default duration if passed value is null

--- app/common/components/Duration/index.tsx
+++ app/common/components/Duration/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 
 import DurationDto from '@app/common/dto/Duration'
+import SimpleDuration from '@app/common/dto/SimpleDuration'
 
 import { formatDuration } from './utils'
 
 interface Props {
   className?: string
-  value: DurationDto
+  value: Nullable<DurationDto>
 }
 
-  const formattedDuration = formatDuration(value.part)
+const SIMPLE_DURATION_ZERO: SimpleDuration = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0
+}
+
 const Duration = ({ className, value }: Props) => {
+  const formattedDuration = formatDuration(value?.part ?? SIMPLE_DURATION_ZERO)
 
   return (
     <span className={className}>
