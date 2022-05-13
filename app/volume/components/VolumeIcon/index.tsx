10351e39e0c2934fb289d77e9e1997704216ffe9
Vasili Karaev <kratos661@gmail.com>
2022-05-13 07:10:26 +0300

    add mute on volume icon click

--- app/volume/components/VolumeIcon/index.tsx
+++ app/volume/components/VolumeIcon/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-
-import * as R from 'ramda'
+import React, { useEffect, useRef } from 'react'
 
 import * as Icons from '@app/common/icons'
 
+import Button from '@app/common/components/Button'
+
 import useStatusContext from '@app/status/use/useStatusContext'
+
+import VolumeService from '@app/volume/services/VolumeService'
 
 import styles from './styles.scss'
 
@@ -23,10 +25,30 @@ const getIcon = (volume: number): React.ElementType => {
 const VolumeIcon = () => {
   const status = useStatusContext()
 
+  const previousVolumeRef = useRef(status.volume)
+
+  useEffect(() => {
+    if (status.volume === 0) {
+      return
+    }
+
+    previousVolumeRef.current = status.volume
+  }, [status.volume])
+
+  const handleClick = () => {
+    const volume = status.volume !== 0
+      ? 0
+      : previousVolumeRef.current
+
+    VolumeService.set(volume)
+  }
+
   const Icon = getIcon(status.volume)
 
   return (
-    <Icon className={styles.icon} />
+    <Button className={styles.button} onClick={handleClick}>
+      <Icon className={styles.icon} />
+    </Button>
   )
 }
 
