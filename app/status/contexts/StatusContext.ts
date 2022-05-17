13778cdaa297d946423323d16895eb4911883c4c
Vasili Karaev <kratos661@gmail.com>
2022-05-17 04:21:10 +0300

    fix deleted module import

--- app/status/contexts/StatusContext.ts
+++ app/status/contexts/StatusContext.ts
@@ -2,8 +2,6 @@ import { createContext } from 'react'
 
 import Status from '@app/status/dto/Status'
 
-import { INITIAL_STATUS } from '@app/status/utils/initial'
-
-const StatusContext = createContext<Status>(INITIAL_STATUS)
+const StatusContext = createContext<Nullable<Status>>(null)
 
 export default StatusContext
