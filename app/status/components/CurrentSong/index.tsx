5f966194a595cd54230352c824d821f187c6e1c1
Vasili Karaev <kratos661@gmail.com>
2022-05-18 02:53:23 +0300

    add current song context menu

--- app/status/components/CurrentSong/index.tsx
+++ app/status/components/CurrentSong/index.tsx
@@ -1,20 +1,33 @@
 import React from 'react'
 
+import * as R from 'ramda'
+
+import PlaylistItem from '@app/playlist/dto/PlaylistItem'
+
 import useCurrentSong from '@app/status/use/useCurrentSong'
+import usePlaylistItemContextMenu from '@app/playlist/use/usePlaylistItemContextMenu'
 
 import styles from './styles.scss'
 
-const CurrentSong = () => {
-  const currentSong = useCurrentSong()
+const CurrentSongPlaceholder = () => (
+  <div className={styles.container}>
+    <span className={styles.title}>
+    -------
+    </span>
+    <span className={styles.artist}>
+    ---
+    </span>
+  </div>
+)
 
-  const title = currentSong?.title
-    ?? '-------'
+const CurrentSong = ({ currentSong }: { currentSong: PlaylistItem }) => {
+  const { handleContextMenu } = usePlaylistItemContextMenu(currentSong)
 
-  const artist = currentSong?.artist
-    ?? '---'
+  const title = currentSong.title
+  const artist = currentSong.artist
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onContextMenu={handleContextMenu}>
       <span className={styles.title}>
       {title}
       </span>
@@ -25,4 +38,18 @@ const CurrentSong = () => {
   )
 }
 
-export default CurrentSong
+const CurrentSongWrapper = () => {
+  const currentSong = useCurrentSong()
+
+  if (R.isNil(currentSong)) {
+    return (
+      <CurrentSongPlaceholder />
+    )
+  }
+
+  return (
+    <CurrentSong currentSong={currentSong} />
+  )
+}
+
+export default CurrentSongWrapper
