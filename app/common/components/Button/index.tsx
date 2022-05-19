bb002c9dbcb820f1f002e9b4ca1a951f63bc9887
Vasili Karaev <kratos661@gmail.com>
2022-05-20 02:38:27 +0300

    blur button on mouse up

--- app/common/components/Button/index.tsx
+++ app/common/components/Button/index.tsx
@@ -11,12 +11,17 @@ interface Props extends React.ComponentProps<'button'> {
 }
 
 const Button  = ({ className, children, onClick, ...props }: Props) => {
+  const handleMouseUp: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.currentTarget.blur()
+  }
+
   return (
     <button
       {...props}
       className={cx(styles.button, className)}
       type="button"
       onClick={onClick}
+      onMouseUp={handleMouseUp}
     >
       {children}
     </button>
