5fd4acc72b61fbba3a6bfdf7d52a43ae0aee7171
Vasili Karaev <kratos661@gmail.com>
2022-05-27 03:29:01 +0300

    replace T | null with Nullable<T>

--- app/database/data/DatabaseFile.ts
+++ app/database/data/DatabaseFile.ts
@@ -3,9 +3,9 @@ import DatabaseItem from '@app/database/data/DatabaseItem'
 import DatabaseAudioFormat from '@app/database/data/DatabaseAudioFormat'
 
 interface DatabaseFile extends DatabaseItem {
-  title: string | null
-  artist: string | null
-  album: string | null
+  title: Nullable<string>
+  artist: Nullable<string>
+  album: Nullable<string>
   duration: Duration
   format: DatabaseAudioFormat
 }
