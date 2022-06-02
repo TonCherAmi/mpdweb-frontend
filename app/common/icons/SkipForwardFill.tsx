f5e7fb17f0c4274e80239ccd3d888eb8ee08c88d
Vasili Karaev <kratos661@gmail.com>
2022-06-02 09:17:39 +0300

    replace bootstrap skip forward fill icon with custom-made one

--- app/common/icons/SkipForwardFill.tsx
+++ app/common/icons/SkipForwardFill.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
 
-/**
- * Altered Bootstrap SVG icon
- * License: https://github.com/twbs/icons/blob/main/LICENSE.md
- */
 const SkipForwardFill = ({ className }: { className: string }) => (
-  <svg className={className} viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path fillRule="evenodd" d="M15.5 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z" />
-    <path d="M7.596 8.697l-6.363 3.692C.693 12.702 0 12.322 0 11.692V4.308c0-.63.693-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-    <path d="M15.096 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.693-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+  <svg className={className} version="1.1" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+    <path d="M28.3438 80.0625C11.4681 79.0557-3.49681e-06 90.8366 0 112.844L0 400.469C0 431.208 23.2514 441.191 51.9375 422.75L256 291.562L256 400.469C256 431.208 276.657 441.191 302.156 422.75L496.656 282.094C517.148 267.274 517.106 243.267 496.594 228.469L304.125 89.625C277.548 70.4523 256 80.8334 256 112.844L256 219.062L54.125 89.625C44.7817 83.6335 36.0145 80.5201 28.3438 80.0625Z" fill="currentColor" />
   </svg>
 )
 
