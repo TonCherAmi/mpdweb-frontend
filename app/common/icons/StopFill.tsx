27b021155772cf721576c96d3a9be637f61687b5
Vasili Karaev <kratos661@gmail.com>
2022-06-02 09:18:50 +0300

    replace bootstrap stop fill icon with custom-made one

--- app/common/icons/StopFill.tsx
+++ app/common/icons/StopFill.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 
-/**
- * Altered Bootstrap SVG icon
- * License: https://github.com/twbs/icons/blob/main/LICENSE.md
- */
 const StopFill = ({ className }: { className: string }) => (
-  <svg className={className} viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path d="M5 3.5h6A1.5 1.5 0 0 1 12.5 5v6a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 11V5A1.5 1.5 0 0 1 5 3.5z" />
+  <svg className={className} version="1.1" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+    <path d="M58.8 0L453.2 0C485.674 0 512 26.3257 512 58.8L512 453.2C512 485.674 485.674 512 453.2 512L58.8 512C26.3257 512 0 485.674 0 453.2L0 58.8C0 26.3257 26.3257 0 58.8 0Z" fill="currentColor" />
   </svg>
 )
 
