6bc64d303dfb6c20f123e468719a8267662265ae
Vasili Karaev <kratos661@gmail.com>
2022-06-02 08:53:37 +0300

    replace bootstrap pause fill icon with custom made one

--- app/common/icons/PauseFill.tsx
+++ app/common/icons/PauseFill.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 
-/**
- * Altered Bootstrap SVG icon
- * License: https://github.com/twbs/icons/blob/main/LICENSE.md
- */
 const PauseFill = ({ className }: { className: string }) => (
-  <svg className={className} viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z" />
+  <svg className={className} version="1.1" viewBox="0 0 416 512" xmlns="http://www.w3.org/2000/svg">
+    <path d="M72 0C32.2355 0 0 32.2355 0 72L0 440C0 479.765 32.2355 512 72 512C111.765 512 144 479.765 144 440L144 72C144 32.2355 111.765 0 72 0ZM344 0C304.235 0 272 32.2355 272 72L272 440C272 479.765 304.235 512 344 512C383.765 512 416 479.765 416 440L416 72C416 32.2355 383.765 0 344 0Z" fill="currentColor" />
   </svg>
 )
 
