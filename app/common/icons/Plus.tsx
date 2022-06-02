47f71534a976864f8440f6d530571cbb5b811bd6
Vasili Karaev <kratos661@gmail.com>
2022-06-02 08:54:20 +0300

    replace font awesome plus icon with custom made one

--- app/common/icons/Plus.tsx
+++ app/common/icons/Plus.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 
-/**
- * Altered Font Awesome SVG icon
- * License: https://fontawesome.com/license/free
- */
 const Plus = ({ className }: { className: string }) => (
-  <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-    <path fill="currentColor" d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z"/>
+  <svg className={className} version="1.1" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+    <path d="M256 0C229.49 0 208 21.4903 208 48L208 208L48 208C21.4903 208 0 229.49 0 256C0 282.51 21.4903 304 48 304L208 304L208 464C208 490.51 229.49 512 256 512C282.51 512 304 490.51 304 464L304 304L464 304C490.51 304 512 282.51 512 256C512 229.49 490.51 208 464 208L304 208L304 48C304 21.4903 282.51 0 256 0Z" fill="currentColor" />
   </svg>
 )
 
