0c79175c8f131476e86d3817396388630456e87f
Vasili Karaev <kratos661@gmail.com>
2022-06-06 08:47:51 +0300

    memoize partitioned queue

--- app/queue/components/Queue/use/usePartitionedQueue.ts
+++ app/queue/components/Queue/use/usePartitionedQueue.ts
@@ -1,3 +1,5 @@
+import { useMemo } from 'react'
+
 import * as R from 'ramda'
 
 import QueueItem from '@app/queue/data/QueueItem'
@@ -14,16 +16,19 @@ const usePartitionedQueue = (): PartitionedQueue => {
   const queue = useQueueContext()
   const status = useStatusContext()
 
-  const queueWithoutCurrentSong = R.reject(
-    R.propEq('position', status.song?.position),
-    queue
-  )
+  return useMemo(() => {
+    const queueWithoutCurrentSong = R.reject(
+      R.propEq('position', status.song?.position),
+      queue
+    )
 
-  const [prev, next] = R.partition((item) => {
-    return item.position < (status.song?.position ?? -1)
-  }, queueWithoutCurrentSong)
+    const [prev, next] = R.partition((item) => {
+      return item.position < (status.song?.position ?? -1)
+    }, queueWithoutCurrentSong)
 
-  return { prev, next }
+    return { prev, next }
+  }, [queue, status.song?.position])
+
 }
 
 export default usePartitionedQueue
