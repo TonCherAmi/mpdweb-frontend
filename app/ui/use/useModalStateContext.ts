a6827eea97beb612f3ef149b0f417c7217cf3a18
Vasili Karaev <kratos661@gmail.com>
2022-06-29 09:10:14 +0300

    remove unused modal code

--- app/ui/use/useModalStateContext.ts
+++
@@ -1,9 +0,0 @@
-import { useContext, ContextType } from 'react'
-
-import ModalStateContext from '@app/ui/contexts/ModalStateContext'
-
-const useModalStateContext = (): ContextType<typeof ModalStateContext> => {
-  return useContext(ModalStateContext)
-}
-
-export default useModalStateContext
