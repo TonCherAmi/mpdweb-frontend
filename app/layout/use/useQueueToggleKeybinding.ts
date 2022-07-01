b97e9ae5bd0ec41e90c609b231e364d9ba48ea63
Vasili Karaev <kratos661@gmail.com>
2022-07-01 09:45:42 +0300

    fix queue focus reset logic

--- app/layout/use/useQueueToggleKeybinding.ts
+++ app/layout/use/useQueueToggleKeybinding.ts
@@ -4,25 +4,31 @@ import * as R from 'ramda'
 
 import useKeybindings from '@app/keybindings/use/useKeybindings'
 import useQueueContext from '@app/queue/use/useQueueContext'
+import useStatusContext from '@app/status/use/useStatusContext'
 import useFocusScopeContext from '@app/ui/use/useFocusScopeContext'
 import useAnyFocusScopeActive from '@app/ui/use/useAnyFocusScopeActive'
 
 const useQueueToggleKeybinding = () => {
   const queue = useQueueContext()
+  const status = useStatusContext()
 
   const [focusScope, dispatchFocusScope] = useFocusScopeContext()
 
   const isApplicableFocusScopeActive = useAnyFocusScopeActive(['queue', 'view'])
 
+  const isQueueUnfocusable = R.isEmpty(queue) || (
+    queue.length <= 1 && !R.isNil(status.song)
+  )
+
   useEffect(() => {
-    if (queue.length <= 1 && focusScope === 'queue') {
+    if (isQueueUnfocusable && focusScope === 'queue') {
       dispatchFocusScope({ type: 'toggle', scope: 'queue' })
     }
-  }, [queue.length, focusScope, dispatchFocusScope])
+  }, [isQueueUnfocusable, focusScope, dispatchFocusScope])
 
   useKeybindings({
     QUEUE_TOGGLE: () => dispatchFocusScope({ type: 'toggle', scope: 'queue' }),
-  }, { disable: R.isEmpty(queue) || !isApplicableFocusScopeActive })
+  }, { disable: isQueueUnfocusable || !isApplicableFocusScopeActive })
 }
 
 export default useQueueToggleKeybinding
