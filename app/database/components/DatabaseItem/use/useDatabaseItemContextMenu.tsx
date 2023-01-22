1ed26cff48a7b787b8b7ba8df459fcffdb404e0b
Vasili Karaev <kratos661@gmail.com>
2023-01-22 12:43:40 +0300

    add update database item context menu item

--- app/database/components/DatabaseItem/use/useDatabaseItemContextMenu.tsx
+++ app/database/components/DatabaseItem/use/useDatabaseItemContextMenu.tsx
@@ -5,15 +5,23 @@ import DatabaseItem from '@app/database/data/DatabaseItem'
 import ContextMenu from '@app/common/components/ContextMenu'
 
 import useContextMenu from '@app/ui/use/useContextMenu'
+import useDatabaseActions from '@app/database/use/useDatabaseActions'
 
 import { wrapWithGlobalContextMenuItems } from '@app/common/utils/contextmenu'
 import { getDatabaseItemContextMenuItems } from '@app/database/utils/contextmenu'
 
 const useDatabaseItemContextMenu = (item: DatabaseItem) => {
+  const { update } = useDatabaseActions()
+
   return useContextMenu((onClose) => {
-    const items = wrapWithGlobalContextMenuItems(
-      getDatabaseItemContextMenuItems(item)
-    )
+    const items = wrapWithGlobalContextMenuItems([
+      ...getDatabaseItemContextMenuItems(item),
+      {
+        id: 'update',
+        text: 'Update',
+        handler: () => update(item.uri),
+      },
+    ])
 
     return (
       <ContextMenu
