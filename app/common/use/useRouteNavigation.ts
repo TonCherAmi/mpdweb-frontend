e140684f2746d924b4617b174ff918dde48c6d08
Vasili Karaev <kratos661@gmail.com>
2023-09-24 22:08:07 +0200

    use correct route type

--- app/common/use/useRouteNavigation.ts
+++ app/common/use/useRouteNavigation.ts
@@ -2,12 +2,12 @@ import { useCallback } from 'react'
 
 import { useHistory } from 'react-router-dom'
 
-import Route from '@app/common/types/Route'
 import Handler from '@app/common/types/Handler'
+import ParametrizedRoute from '@app/common/types/ParametrizedRoute'
 
 import { joinPath } from '@app/common/utils/path'
 
-const useRouteNavigation = <T extends string> (route: Route<T>): Handler<string> => {
+const useRouteNavigation = <T extends string> (route: ParametrizedRoute<T>): Handler<string> => {
   const history = useHistory()
 
   return useCallback((path) => {
