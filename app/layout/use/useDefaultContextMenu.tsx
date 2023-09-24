95aeab47b43f7b6255313e333ffd25513a4969b4
Vasili Karaev <kratos661@gmail.com>
2023-09-24 23:44:46 +0200

    add more default context menu items

--- app/layout/use/useDefaultContextMenu.tsx
+++ app/layout/use/useDefaultContextMenu.tsx
@@ -3,10 +3,26 @@ import React from 'react'
 import ContextMenu from '@app/common/components/ContextMenu'
 
 import useContextMenu from '@app/ui/use/useContextMenu'
+import useQueueActions from '@app/queue/use/useQueueActions'
+import useDatabaseActions from '@app/database/use/useDatabaseActions'
 
 const useDefaultContextMenu = () => {
+  const { clear } = useQueueActions()
+  const { update } = useDatabaseActions()
+
   return useContextMenu((onClose) => {
-    const items = []
+    const items = [
+      {
+        id: 'clear-queue',
+        text: 'Clear Queue',
+        handler: clear,
+      },
+      {
+        id: 'update-database',
+        text: 'Update Database',
+        handler: update,
+      },
+    ]
 
     return (
       <ContextMenu
